7034d8f217e4ca903eefea1a693af6ab071a9943
Evelyn Nguyen <enguyen6208@gmail.com>
2024-06-28 08:28:49 -0800

    layout children type definition

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,7 +1,12 @@
+import React, { ReactNode } from 'react';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+
+interface LayoutProps {
+  children: ReactNode;
+}
  
-export default function Layout({ children }) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Navbar />
