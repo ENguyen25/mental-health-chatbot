a81cbcbfd202a560b2e24d59369fe97d8f4c25dc
Evelyn Nguyen <enguyen6208@gmail.com>
2024-06-30 11:18:13 -0800

    header + navbar styling

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
 export default function Navbar() {
+
+  useEffect(() => {
+    const handleScroll = () => {
+        const nav = document.querySelector(".navbar");
+        if (!nav) return;
+
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+        if (scrollTop > nav.clientHeight) {
+            nav.classList.add('scrolled');
+        } else {
+            nav.classList.remove('scrolled');
+        }
+    };
+
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+        document.removeEventListener('scroll', handleScroll);
+    };
+}, []);
+
     return (
-      <div className="w-full flex justify-center py-5">
-        <nav className="w-[1000px] flex items-end">
+      <div className="navbar fixed w-full flex justify-center py-5">
+        <nav className="w-[1000px] flex items-center">
             <p className="w-1/6 font-bold">CLARITY</p>
             <ul className="w-4/6 flex gap-8">
                 <li>Features</li>
--- pages/components/landing/Header.tsx
+++ pages/components/landing/Header.tsx
@@ -2,15 +2,17 @@ import Image from "next/image"
 
 export default function Header() {
     return (
-        <div className="header w-full flex justify-center py-20">
-            <div className="w-[1050px] flex justify-between items-center">
-                <Image src="/images/self-love.png" alt="mobile app" width={300} height={300} />
-                <div className="w-[400px] flex flex-col items-center pb-10 text-center">
-                    <h1 className="mb-5">Never be lonely again.</h1>
-                    <p className="text-2xl">Our app provides all the resources you need to take care of yourself in the moment at no cost.</p>
-                    <button className="pink-gradient-button text-xl w-fit mt-8">Sign Up Now</button>
+        <div className="header-mask w-full">
+            <div className="header w-full flex justify-center items-end py-48">
+                <div className="w-[1050px] flex justify-between items-center">
+                    <Image src="/images/self-love.png" alt="mobile app" width={300} height={300} />
+                    <div className="w-[400px] flex flex-col items-center pb-10 text-center">
+                        <h1 className="mb-5">Never be lonely again.</h1>
+                        <p className="text-2xl">Our app provides all the resources you need to take care of yourself in the moment at no cost.</p>
+                        <button className="pink-gradient-button text-xl w-fit mt-8">Sign Up Now</button>
+                    </div>
+                    <Image src="/images/self-love-mirror.png" alt="mobile app" width={300} height={300} />
                 </div>
-                <Image src="/images/self-love-mirror.png" alt="mobile app" width={300} height={300} />
             </div>
         </div>
     )
